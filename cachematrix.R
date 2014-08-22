052826060d95b09f15dd1c381cdaa8de2b039cac
AR-datascience <solaris0112@gmail.com>
2014-08-23 01:43:43 +0200

    my implementation of cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -1,17 +1,56 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The two functions below calculate matrix inversion using cached values
 
-## Write a short comment describing this function
 
-## This is the first commit (AR)
+## Creates a matrix that caches its inverse
+makeCacheMatrix <- function (mat = matrix()) {
 
-makeCacheMatrix <- function(x = matrix()) {
+    inv <- NULL
+## Method to set the value of the matrix
+    set <- function (matrix) {
+            mat <<- matrix
+            inv <<- NULL
+    }
 
+## Method the get the matrix
+    get <- function() {
+    	mat
+    }
+
+## Method to set the inverse of the matrix
+    setInverse <- function (inverse) {
+        inv <<- inverse
+    }
+
+## Method to get the inverse of the matrix
+    getInverse <- function() {
+        inv
+    }
+
+## Return the list of methods
+    list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## Compute the inverse of the matrix.
+## If the inverse has already been calculated it simply retrieves the inverse
+## from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function (x, ...) {
+
+## get the inverse of matrix x using the method provided by the function above 
+    inv_x <- x$getInverse()
+
+## if the returned value is not null, i.e. inverse already calculated, 
+## returns the "inv" value
+    if( !is.null(inv_x) ) {
+        return(inv_x)
+    }
+    else {
+## Calculate the inverse of the given matrix using the "solve" function
+         inv_x <- solve(x$get()) 
+
+## Set the inverse to the calculated value
+         x$setInverse(inv_x)
+         return(inv_x)
+    }
 }
